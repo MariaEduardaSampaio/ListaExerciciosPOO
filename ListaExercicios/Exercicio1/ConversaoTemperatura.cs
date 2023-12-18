86488fa6b97115912b3a321281e1eba36216d402
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2023-12-18 19:18:19 -0300

    adiciona primeiro exercicio

---
+++ ListaExercicios/Exercicio1/ConversaoTemperatura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.Exercicio1
+{
+    public static class ConversaoTemperatura
+    {
+        public static decimal ConverterCelsiusParaFahrenheit(decimal tempCelsius)
+        {
+            return ((9/5) * tempCelsius + 32);
+        }
+
+        public static decimal ConverterFahrenheitParaCelsius(decimal tempFahrenheit)
+        {
+            return ((5/9) * (tempFahrenheit - 32));
+        }
+    }
+}
