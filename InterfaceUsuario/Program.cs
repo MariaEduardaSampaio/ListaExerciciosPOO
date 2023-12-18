f449528926d26306e95a6f6e64c6e7eaad31d1e3
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2023-12-18 19:18:58 -0300

    estrutura projeto

---
+++ InterfaceUsuario/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace InterfaceUsuario
+{
+    internal class Program
+    {
+        static void ListarExercicios() 
+        {
+            Console.WriteLine("1. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("2. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("3. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("4. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("5. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("6. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("7. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("8. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("9. Conversão de temperatura entre celsius e fahrenheit.");
+            Console.WriteLine("10. Conversão de temperatura entre celsius e fahrenheit.");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
